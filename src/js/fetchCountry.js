b5feb709c7f1ea545fd610a1335091d94c473c98
Maksim Tsopa <maks98242@gmail.com>
2021-05-19 20:32:45 +0300

    prime ver

--- src/js/fetchCountry.js
+++ src/js/fetchCountry.js
@@ -1,52 +1,19 @@
-// import countrysTamplate from '../templates/countries.hbs';
-// import countryTamplate from '../templates/country.hbs';
-
-// const refs = {
-//   textInput: document.querySelector('.searchCountry'),
-//   country: document.querySelector('.js-country'),
-// };
-
-// const debounce = require('lodash.debounce');
-
-
-// refs.textInput.addEventListener('input', debounce(findCountry, 500));
-
-// function findCountry() {
-//   fetchQuery(refs.textInput.value).then(renderCountry).catch(console.error());
-// }
+import { alert } from '@pnotify/core';
 
 export default fetchQuery;
 
 function fetchQuery(countryId) {
   return fetch(`https://restcountries.eu/rest/v2/name/${countryId}`).then(
     response => {
+      if (response.status === 404) {
+        alert({
+          type: 'error',
+          text: 'Error 404!',
+          styling: 'brighttheme',
+          mode: 'light',
+        });
+      }
       return response.json();
     },
   );
 }
-
-// function renderCountry(country) {
-//   refs.country.innerHTML = '';
-//   if (country.length === 1) {
-//     const newCountry = country[0];
-//     console.log(country);
-//     console.log(newCountry);
-//     console.log(newCountry.name);
-//     console.log(newCountry.capital);
-//     console.log(newCountry.population);
-//     newCountry.languages.map(language => console.log(language.name));
-//     console.log(newCountry.flag);
-//     refs.country.insertAdjacentHTML('beforeend', countryTamplate(newCountry));
-//   }
-//   if (country.length <= 10 && country.length !== 1) {
-//     console.log('+');
-//     refs.country.insertAdjacentHTML(
-//       'beforeend',
-//       countrysTamplate(country.map(countrys => countrys.name)),
-//     );
-//   }
-//   if (country.length > 10) {
-//     console.log('-');
-//   }
-// }
-
--- src/js/renderCountry.js
+++ src/js/renderCountry.js
@@ -1,7 +1,11 @@
 import countrysTamplate from '../templates/countries.hbs';
 import countryTamplate from '../templates/country.hbs';
-
 import fetchQuery from './fetchCountry';
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert } from '@pnotify/core';
+
 
 const debounce = require('lodash.debounce');
 
@@ -9,7 +13,6 @@ const refs = {
   textInput: document.querySelector('.searchCountry'),
   country: document.querySelector('.js-country'),
 };
-
 
 refs.textInput.addEventListener('input', debounce(findCountry, 500));
 
@@ -21,23 +24,20 @@ function renderCountry(country) {
   refs.country.innerHTML = '';
   if (country.length === 1) {
     const newCountry = country[0];
-    console.log(country);
-    console.log(newCountry);
-    console.log(newCountry.name);
-    console.log(newCountry.capital);
-    console.log(newCountry.population);
-    newCountry.languages.map(language => console.log(language.name));
-    console.log(newCountry.flag);
     refs.country.insertAdjacentHTML('beforeend', countryTamplate(newCountry));
   }
   if (country.length <= 10 && country.length !== 1) {
-    console.log('+');
     refs.country.insertAdjacentHTML(
       'beforeend',
       countrysTamplate(country.map(countrys => countrys.name)),
     );
   }
   if (country.length > 10) {
-    console.log('-');
+    alert({
+      type: 'error',
+      text: 'Too many matches found. Please enter a more specific query!',
+      styling: 'brighttheme',
+      mode: 'light',
+    });
   }
 }
